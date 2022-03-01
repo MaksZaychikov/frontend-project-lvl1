28efbe7f4c45d1c447117bec0f0d8a38b2e5c656
Zaychikov Maks <zaychikov.maks@gmail.com>
2022-03-01 14:09:20 +0400

    update 3 step

---
+++ bin/src/cli.js
@@ -0,0 +1,8 @@
+import readLineSync from 'readline-sync';
+
+ const userName = () => {
+    const name = readLineSync.question('May I have your name? ');
+    console.log( `Hello, ${name}!`);
+};
+export default userName;
+userName();
