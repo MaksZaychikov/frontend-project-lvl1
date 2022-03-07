f44e3f1664505d11932a2ebd22e813edb5b94565
Zaychikov Maks <zaychikov.maks@gmail.com>
2022-03-07 13:39:11 +0400

    last update

--- bin/src/cli.js
+++
@@ -1,8 +0,0 @@
-import readLineSync from 'readline-sync';
-
- const userName = () => {
-    const name = readLineSync.question('May I have your name? ');
-    console.log( `Hello, ${name}!`);
-};
-export default userName;
-userName();
--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,7 @@
-#!/usr/bin/env node
 import readLineSync from 'readline-sync';
 
 const userName = () => {
   const name = readLineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
-export default userName;
-console.log('Welcome to the Brain Games!')
-
-userName();
+export default userName; 
