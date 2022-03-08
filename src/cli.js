4b5b1881939394dd0a98a0fcc5e4f7bbd084c7a3
Zaychikov Maks <zaychikov.maks@gmail.com>
2022-03-08 18:21:50 +0400

    refactored  cli.js.

--- src/cli.js
+++ src/cli.js
@@ -4,40 +4,4 @@ const userName = () => {
   const name = readLineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
-
-
-const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-// eslint-disable-next-line no-unused-vars
-const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
-const isEven = (num) => num % 2 === 0;
-// eslint-disable-next-line no-unused-vars
-const gameCheck = () => {
-  const question = getRandomNum(1, 100);
-  const correctAnswer = isEven(question) ? 'yes' : 'no';
-  return [question, correctAnswer];
-};
-const roundCount = 3;
-
-// eslint-disable-next-line no-shadow
-const runGame = (gameDescription, getGameCheck) => {
-  console.log('Welcome to the Brain Games!');
-  const name = readLineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log(gameDescription);
-
-  for (let i = 1; i <= roundCount; i += 1); {
-    const [question, correctAnswer] = getGameCheck();
-    console.log(`Question: ${question}`);
-    const userAnswer = readLineSync.question('Your answer: ');
-    if (correctAnswer === userAnswer) {
-      console.log('Correct');
-    // eslint-disable-next-line no-lone-blocks
-    } else {
-      console.log(`${userAnswer} is wrong answer;(. Correct answer was ${correctAnswer}. \nLet's try again, ${name}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${name}!`);
-};
-const startGame = () => runGame(gameDescription, gameCheck);
-export default startGame;
+export default userName;
