8bb8a6c38726557ffec488c479b0914a6ec5eae7
Zaychikov Maks <zaychikov.maks@gmail.com>
2022-03-10 12:59:02 +0400

    finished brain-calc

--- Makefile
+++ Makefile
@@ -10,5 +10,8 @@ publish:
 brain-even:
 	node ./bin/brain-even.js
 
+brain-calc:
+	node ./bin/brain-calc.js
+
 lint:
 	npx eslint .
---
+++ src/games/calc-logic.js
@@ -0,0 +1,30 @@
+import runGame from '../runGame.js';
+import getRandomNum from '../getRandom.js';
+
+const gameDescription = 'What is the result of the expression?';
+
+const calc = (first, second, operation) => {
+  let result;
+  if (operation === '+') {
+    result = first + second;
+  } else if (operation === '-') {
+    result = first - second;
+  } else {
+    result = first * second;
+  }
+  return result;
+};
+
+const gameData = () => {
+  const number1 = getRandomNum(1, 50);
+  const number2 = getRandomNum(1, 50);
+  const operators = ['-', '+', '*'];
+  const operator = operators[getRandomNum(0, operators.length - 1)];
+  const question = `${number1} ${operator} ${number2}`;
+  const correctAnswer = calc(number1, number2, operator).toString();
+  return [question, correctAnswer];
+};
+
+const startGame = () => runGame(gameDescription, gameData);
+
+export default startGame;
