05dc385cf564f570241b674c443a484ba24d0f2c
Zaychikov Maks <zaychikov.maks@gmail.com>
2022-03-10 10:57:39 +0400

    chaged functions

--- Makefile
+++ Makefile
@@ -7,5 +7,8 @@ brain-games:
 publish:
 	 npm publish --dry-run
 
+brain-even:
+	node ./bin/brain-even.js
+
 lint:
 	npx eslint .
---
+++ src/runGame.js
@@ -0,0 +1,24 @@
+import readLineSync from 'readline-sync';
+
+const roundsCount = 3;
+
+const runGame = (gameDescr, getGameData) => {
+  console.log('Welcome to the Brain Games!');
+  const name = readLineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log(gameDescr);
+  for (let i = 1; i <= roundsCount; i += 1) {
+    const [question, correctAnswer] = getGameData();
+    console.log(`Question: ${question}`);
+    const answer = readLineSync.question('Your answer: ');
+    if (correctAnswer === answer) {
+      console.log('Correct');
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}. \nLet's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default runGame;
